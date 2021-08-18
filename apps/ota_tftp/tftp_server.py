5a2b4fa3ee7a02dc513dd4e551f62149ddccbf58
Alexey Tsvetkov <xeenych@gmail.com>
2021-08-18 14:15:57 +0300

    1

--- apps/ota_tftp/tftp_server.py
+++
@@ -1,4 +0,0 @@
-import tftpy
-
-server = tftpy.TftpServer(tftproot='.', dyn_file_func=None)
-server.listen(listenport=69, timeout=10)
--- setenv.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@echo setting enviroment variables
-set PATH=C:\cygwin64\bin\;%PATH%
-set ARM_GCC_TOOLCHAIN="C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2020-q4-major/bin/"
